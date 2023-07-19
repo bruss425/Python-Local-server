f0c763443789048d03e418c2f26dfc1217249458
bruss425 <russellben108@gmail.com>
2023-07-19 13:49:57 -0500

    changed /account

--- website/views.py
+++ website/views.py
@@ -42,16 +42,55 @@ def delete_note():
 
     return jsonify({})
 
-@views.route('/account', methods=['GET', 'POST'])
+@views.route('/account', methods=['GET'])
 @login_required
-def account():
-    global balance
+def account_home():
     user = User.query.get(current_user.id)
     balance = user.balance1
+    return render_template("account/base.html", user=current_user, balance = balance)
+
+@views.route('/account/deposit', methods=['GET', 'POST'])
+@login_required
+def deposit():
+    global balance
+    user = User.query.get(current_user.id)
+    balance = (int)(user.balance1)
     if request.method == 'POST': 
-        new_balance = (request.form.get('balance'))
-        user.balance1 = new_balance
-        db.session.commit()
-        flash('Your account balance has been changed.', category='success')
-        balance = user.balance1
-    return render_template("account.html", user=current_user, balance = balance)
+        try:
+            new_balance = (int)(request.form.get('balance'))
+        except:
+             flash('Your input was not a number amount.', category='error')
+        else: 
+            user.balance1 = new_balance + balance
+            db.session.commit()
+            flash(('You have deposited $' + str(new_balance) + ".00 into your account."), category='success')
+            balance = user.balance1
+       
+    return render_template("account/deposit.html", user=current_user, balance = balance)
+
+@views.route('/account/withdraw', methods=['GET','POST'])
+@login_required
+def withdraw():
+    global balance
+    user = User.query.get(current_user.id)
+    balance = (int)(user.balance1)
+    if request.method == 'POST': 
+        try:
+            new_balance = (int)(request.form.get('balance'))
+        except:
+             flash('Your input was not a number amount.', category='error')
+        else:
+            if((balance - new_balance) < 0):
+                flash('Your maximum amount to withdraw is $' + str(balance) + '.00', category='error')
+            else: 
+                user.balance1 = balance - new_balance
+                db.session.commit()
+                flash(('You have withdrawn $' + str(new_balance) + ".00 from your account."), category='success')
+                balance = user.balance1
+        
+    return render_template("account/withdraw.html", user=current_user, balance = balance)
+
+
+ 
+
+#api is next! 
